e9851af47e2eb03e696ec271aef3975ed8005827
Jimmy Chen <jimc1682000@gmail.com>
2021-09-22 09:25:42 +0000

    Use cookbook_file to create static file

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -8,13 +8,17 @@ package 'httpd' do
   action :install
 end
 
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
-  variables(
-    :name => 'Jimmy Chen'
-  )
-  action :create
+cookbook_file '/var/www/html/index.html' do
+  source 'index.html'
 end
+
+#template '/var/www/html/index.html' do
+#  source 'index.html.erb'
+#  variables(
+#    :name => 'Jimmy Chen'
+#  )
+#  action :create
+#end
 
 service 'httpd' do
   action [ :enable, :start ]
