c29368168a65ef4080cac092b4d213860b373227
Jimmy Chen <jimc1682000@gmail.com>
2021-09-22 08:57:40 +0000

    Use template in setup.rb

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -9,21 +9,7 @@ end
 package 'git'
 package 'ntp'
 
-#node['ipaddress']
-#node['memory']['total']
-
-# pritn statement 'I have 4 apples'
-#apple_count = 4
-#puts "I have #{apple_count} apples"
-
-file '/etc/motd' do
-  content "This server is the property of Jimmy
-  HOSTNAME: #{node['hostname']}
-  IPADDRESS: #{node['ipaddress']}
-  CPU: #{node['cpu']['0']['mhz']}
-  MEMORY: #{node['memory']['total']}
-"
+template '/etc/motd' do
+  source 'motd.erb' 
   action :create
-  owner 'root'
-  group 'root'
 end
