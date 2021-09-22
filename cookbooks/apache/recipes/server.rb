59eced58509507fe0e2e8d0e2aba3f8187545c77
Jimmy Chen <jimc1682000@gmail.com>
2021-09-22 09:09:35 +0000

    Use template for index.html

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -8,11 +8,12 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello World!</h1>
-  <h2>IP address: #{node['ipaddress']}</h2>
-  <h2>Hostname: #{node['hostname']}</h2>
-"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  variables(
+    :name => 'Jimmy Chen'
+  )
+  action :create
 end
 
 service 'httpd' do
