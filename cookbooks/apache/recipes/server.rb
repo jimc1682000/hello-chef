f2888418f2690beabc3bad7565dcb41567777792
Jimmy Chen <jimc1682000@gmail.com>
2021-09-22 09:47:02 +0000

    Use directory to replace execute and bash

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -24,6 +24,37 @@ template '/var/www/html/index.html' do
   action :create
 end
 
+#bash 'inline script' do
+#  user 'root'
+#  code 'mkdir -p /var/www/mysites/ && chown -R apache /var/www/mysites/'
+#  #not_if '[ -d /var/www/mysites/ ]'
+#  # Ruby style
+#  not_if do
+#    File.directory?('/var/www/mysites/')
+#  end
+#end
+
+#execute 'run a script' do
+#  user 'root'
+#  command <<-EOH
+#  mkdir -p /var/www/mysites/
+#  chown -R apache /var/www/mysites/
+#  EOH
+#  not_if
+#end
+
+#execute 'run script' do
+#  user 'root'
+#  command './myscript.sh'
+#  not_if
+#end
+
+directory '/var/www/mysites' do
+  owner 'apache'
+  recursive true
+  mode '0644'
+end
+
 service 'httpd' do
   action [ :enable, :start ]
 end
