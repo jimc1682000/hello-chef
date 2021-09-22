1c2ddf9887e9e7207bb59c23ad286eeb064e78a5
Jimmy Chen <jimc1682000@gmail.com>
2021-09-22 08:32:29 +0000

    Access node attributes and add to index.html

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -9,7 +9,10 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello World!</h1>'
+  content "<h1>Hello World!</h1>
+  <h2>IP address: #{node['ipaddress']}</h2>
+  <h2>Hostname: #{node['hostname']}</h2>
+"
 end
 
 service 'httpd' do
