22a27d8218f331fc36539ced6346563019e00c85
Jimmy Chen <jimc1682000@gmail.com>
2021-09-24 18:13:01 +0800

    Use query to dynamic modify servers

--- cookbooks/myhaproxy/recipes/default.rb
+++ cookbooks/myhaproxy/recipes/default.rb
@@ -13,14 +13,29 @@ haproxy_frontend 'http-in' do
   default_backend 'servers'
 end
 
-haproxy_backend 'servers' do
-  server [
-    'web1 192.168.10.43:80 maxconn 32',
-    'web2 192.168.10.44:80 maxconn 32'
-  ]
-  # notifies :reload, 'haproxy_service[haproxy]', :immediately
-  # https://github.com/sous-chefs/haproxy/issues/274
+# search('index', 'search:pattern')
+# Indexes: client, node, role, environment
+
+all_web_nodes = search('node', 'role:web')
+
+servers = []
+
+all_web_nodes.each do |web_node|
+  server = "#{web_node['hostname']} #{web_node['ipaddress']}:80 maxconn 32"
+  servers.push(server)
 end
+
+haproxy_backend 'servers' do
+  server servers
+end
+# haproxy_backend 'servers' do
+#   server [
+#     'web1 192.168.10.43:80 maxconn 32',
+#     'web2 192.168.10.44:80 maxconn 32'
+#   ]
+#   # notifies :reload, 'haproxy_service[haproxy]', :immediately
+#   # https://github.com/sous-chefs/haproxy/issues/274
+# end
 
 haproxy_service 'haproxy' do
   action %i(create enable start)
